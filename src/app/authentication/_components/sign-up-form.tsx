9738ba2e9750b0c07feb39b27c2139fa0791dc28
Chiqueto <lfchiqueto@gmail.com>
2025-05-30 14:55:36 -0300

    feat: add error message for already exists user

--- src/app/authentication/_components/sign-up-form.tsx
+++ src/app/authentication/_components/sign-up-form.tsx
@@ -50,68 +50,68 @@ const SignUpForm = () => {
     }
     return ( 
         <Card>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
 
-                            <CardHeader>
-                                <CardTitle>Criar conta</CardTitle>
-                                <CardDescription>
-                                    Crie uma conta para continuar.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="space-y-4">
-                                <FormField
-                                    control={form.control}
-                                    name="name"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Nome</FormLabel>
-                                            <FormControl>
-                                                <Input placeholder="Digite seu nome..." {...field} />
-                                            </FormControl>
+                    <CardHeader>
+                        <CardTitle>Criar conta</CardTitle>
+                        <CardDescription>
+                            Crie uma conta para continuar.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Nome</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Digite seu nome..." {...field} />
+                                    </FormControl>
 
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="email"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>E-mail</FormLabel>
-                                            <FormControl>
-                                                <Input placeholder="digite seu email..." {...field} />
-                                            </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>E-mail</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="digite seu email..." {...field} />
+                                    </FormControl>
 
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="password"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Senha</FormLabel>
-                                            <FormControl>
-                                                <Input type="password" placeholder="digite sua senha..." {...field} />
-                                            </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Senha</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="digite sua senha..." {...field} />
+                                    </FormControl>
 
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </CardContent>
-                            <CardFooter>
-                                <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
-                                    {form.formState.isSubmitting ? <Loader2 className="w-4 h-4 mr-2 animate-spin" /> : "Criar conta"}
-                                </Button>
-                            </CardFooter>
-                        </form>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                    <CardFooter>
+                        <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>
+                            {form.formState.isSubmitting ? <Loader2 className="w-4 h-4 mr-2 animate-spin" /> : "Criar conta"}
+                        </Button>
+                    </CardFooter>
+                </form>
 
-                     </Form>
-                </Card>
+            </Form>
+        </Card>
      );
 }
  
