5acadab4a4cbe07b08726552b32fce03560cc635
Chiqueto <lfchiqueto@gmail.com>
2025-05-30 14:51:06 -0300

    feat: add login

--- src/app/authentication/_components/login-form.tsx
+++ src/app/authentication/_components/login-form.tsx
@@ -15,6 +15,10 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
 
 const loginSchema = z.object({
     email: z.string().trim().email({ message: "Email inválido" }).min(1, { message: "Email é obrigatório" }),
@@ -22,6 +26,8 @@ const loginSchema = z.object({
 })
 
 const LoginForm = () => {
+
+    const router = useRouter()
     const form = useForm<z.infer<typeof loginSchema>>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
@@ -30,57 +36,68 @@ const LoginForm = () => {
         },
     })
 
-    function onSubmit(values: z.infer<typeof loginSchema>) {
-        console.log(values)
+    async function onSubmit(values: z.infer<typeof loginSchema>) {
+        await authClient.signIn.email({
+            email: values.email,
+            password: values.password,
+        }, {
+                onSuccess:() => {
+                    router.push("/dashboard")
+                },
+                onError: () => {
+                    toast.error("Email ou senha inválidos!")
+                }
+            }
+        )
     }
     return ( 
         <Card>
-                    <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
 
-                            <CardHeader>
-                                <CardTitle>Login</CardTitle>
-                                <CardDescription>
-                                    Faça login para continuar para continuar.
-                                </CardDescription>
-                            </CardHeader>
-                            <CardContent className="space-y-4">
-                                <FormField
-                                    control={form.control}
-                                    name="email"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>E-mail</FormLabel>
-                                            <FormControl>
-                                                <Input placeholder="digite seu email..." {...field} />
-                                            </FormControl>
+                    <CardHeader>
+                        <CardTitle>Login</CardTitle>
+                        <CardDescription>
+                            Faça login para continuar para continuar.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>E-mail</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="digite seu email..." {...field} />
+                                    </FormControl>
 
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                                <FormField
-                                    control={form.control}
-                                    name="password"
-                                    render={({ field }) => (
-                                        <FormItem>
-                                            <FormLabel>Senha</FormLabel>
-                                            <FormControl>
-                                                <Input type="password" placeholder="digite sua senha..." {...field} />
-                                            </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Senha</FormLabel>
+                                    <FormControl>
+                                        <Input type="password" placeholder="digite sua senha..." {...field} />
+                                    </FormControl>
 
-                                            <FormMessage />
-                                        </FormItem>
-                                    )}
-                                />
-                            </CardContent>
-                            <CardFooter>
-                                <Button type="submit" className="w-full">Entrar</Button>
-                            </CardFooter>
-                        </form>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                    <CardFooter>
+                        <Button type="submit" className="w-full" disabled={form.formState.isSubmitting}>{form.formState.isSubmitting ? <Loader2 className="w-4 h-4 mr-2 animate-spin" /> : "Entrar"}</Button>
+                    </CardFooter>
+                </form>
 
-                     </Form>
-                </Card>
+            </Form>
+        </Card>
      );
 }
  
