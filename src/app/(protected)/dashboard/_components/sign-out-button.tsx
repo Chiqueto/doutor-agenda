d8d455e3b0b88068253e3443640044f3ad80b9c9
Chiqueto <lfchiqueto@gmail.com>
2025-05-31 12:09:47 -0300

    feat: add sidebar

---
+++ src/app/(protected)/dashboard/_components/sign-out-button.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+const SignOutButton = () => {
+  const handleSignOut = () =>
+    authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/authentication");
+          toast.success("Deslogado com sucesso");
+        },
+      },
+    });
+  const router = useRouter();
+  return <Button onClick={handleSignOut}>Sair</Button>;
+};
+
+export default SignOutButton;
