6ef511eedb96af22b7a9d9f7cebf314663b63e7a
Chiqueto <lfchiqueto@gmail.com>
2025-06-01 23:45:11 -0300

    feat: ensure patient can be deleted

--- src/app/(protected)/patients/_components/table-actions.tsx
+++ src/app/(protected)/patients/_components/table-actions.tsx
@@ -1,20 +1,34 @@
-import { Button } from "@/components/ui/button";
+import { EditIcon, Trash2Icon } from "lucide-react";
+import { MoreVerticalIcon } from "lucide-react";
+import { useAction } from "next-safe-action/hooks";
+import { useState } from "react";
+import { toast } from "sonner";
 
+import { deletePatient } from "@/actions/delete-patient";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
 import {
   DropdownMenu,
-  DropdownMenuLabel,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
   DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { EditIcon, Trash2Icon } from "lucide-react";
-
-import { Dialog } from "@/components/ui/dialog";
-import { MoreVerticalIcon } from "lucide-react";
 import { patientsTable } from "@/db/schema";
+
 import UpsertPatientForm from "./upsert-patient-form";
-import { useState } from "react";
 interface PatientTableActionsProps {
   patient: typeof patientsTable.$inferSelect;
 }
@@ -22,34 +36,72 @@ interface PatientTableActionsProps {
 const PatientTableActions = ({ patient }: PatientTableActionsProps) => {
   const [upsertPatientFormIsOpen, setUpsertPatientFormIsOpen] = useState(false);
 
+  const deletePatientAction = useAction(deletePatient, {
+    onSuccess: () => {
+      toast.success("Paciente deletado com sucesso");
+    },
+    onError: () => {
+      toast.error("Erro ao deletar paciente");
+    },
+  });
+
+  const handleDeletePatientClick = () => {
+    deletePatientAction.execute({
+      id: patient.id,
+    });
+  };
+
   return (
-    <Dialog
-      open={upsertPatientFormIsOpen}
-      onOpenChange={setUpsertPatientFormIsOpen}
-    >
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button variant="ghost" size="icon">
-            <MoreVerticalIcon className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => setUpsertPatientFormIsOpen(true)}>
-            <EditIcon /> Editar
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Trash2Icon /> Excluir
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <UpsertPatientForm
-        isOpen={upsertPatientFormIsOpen}
-        patient={patient}
-        onSuccess={() => setUpsertPatientFormIsOpen(false)}
-      />
-    </Dialog>
+    <>
+      <Dialog
+        open={upsertPatientFormIsOpen}
+        onOpenChange={setUpsertPatientFormIsOpen}
+      >
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="ghost" size="icon">
+              <MoreVerticalIcon className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>{patient.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => setUpsertPatientFormIsOpen(true)}>
+              <EditIcon /> Editar
+            </DropdownMenuItem>
+            <AlertDialog>
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                  <Trash2Icon /> Excluir
+                </DropdownMenuItem>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>
+                    Tem certeza que deseja deletar este paciente?
+                  </AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Essa ação não pode ser revertida. Isso irá deletar o
+                    paciente permanentemente.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                  <AlertDialogAction onClick={handleDeletePatientClick}>
+                    Deletar
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <UpsertPatientForm
+          isOpen={upsertPatientFormIsOpen}
+          patient={patient}
+          onSuccess={() => setUpsertPatientFormIsOpen(false)}
+        />
+      </Dialog>
+    </>
   );
 };
 
--- src/app/(protected)/patients/_components/table-colums.tsx
+++ src/app/(protected)/patients/_components/table-colums.tsx
@@ -1,57 +1,47 @@
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { patientsTable } from "@/db/schema";
-import { Button } from "@/components/ui/button";
-import { EditIcon, MoreVerticalIcon, Trash2Icon } from "lucide-react";
-import { Dialog } from "@/components/ui/dialog";
+
 import PatientTableActions from "./table-actions";
 
 type Patient = typeof patientsTable.$inferSelect;
 
 export const patientsTableColumns: ColumnDef<Patient>[] = [
-  {
-    id: "name",
-    accessorKey: "name",
-    header: "Nome",
-  },
-  {
-    id: "email",
-    accessorKey: "email",
-    header: "E-mail",
-  },
-  {
-    id: "phoneNumber",
-    accessorKey: "phoneNumber",
-    header: "Telefone",
-    cell: (params) => {
-      const phone = params.row.original.phoneNumber;
-      if (!phone) return "-";
-      return `(${phone.slice(0, 2)}) ${phone.slice(2, 7)}-${phone.slice(7)}`;
+    {
+        id: "name",
+        accessorKey: "name",
+        header: "Nome",
     },
-  },
-  {
-    id: "sex",
-    accessorKey: "sex",
-    header: "Gênero",
-    cell: (params) => {
-      const patient = params.row.original;
-      return patient.sex === "male" ? "Masculino" : "Feminino";
+    {
+        id: "email",
+        accessorKey: "email",
+        header: "E-mail",
     },
-  },
-  {
-    id: "actions",
-    cell: (params) => {
-      const patient = params.row.original;
-      return <PatientTableActions patient={patient} />;
+    {
+        id: "phoneNumber",
+        accessorKey: "phoneNumber",
+        header: "Telefone",
+        cell: (params) => {
+            const phone = params.row.original.phoneNumber;
+            if (!phone) return "-";
+            return `(${phone.slice(0, 2)}) ${phone.slice(2, 7)}-${phone.slice(7)}`;
+        },
     },
-  },
+    {
+        id: "sex",
+        accessorKey: "sex",
+        header: "Gênero",
+        cell: (params) => {
+            const patient = params.row.original;
+            return patient.sex === "male" ? "Masculino" : "Feminino";
+        },
+    },
+    {
+        id: "actions",
+        cell: (params) => {
+            const patient = params.row.original;
+            return <PatientTableActions patient={patient} />;
+        },
+    },
 ];
